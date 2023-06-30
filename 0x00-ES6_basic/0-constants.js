0c80be7a8d6d09666277921027b1d48563a26d1f
Babafrankie <fnwandibie@gmail.com>
2023-06-30 03:25:40 +0000

    ES6 arrow functions

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,19 +1,15 @@
-export function taskFirst()
-{
-	const task = 'I prefer
-const when I can.';
-	return task;
+export function taskFirst() {
+  const task = 'I prefer const when I can.';
+  return task;
 }
 
 export function getLast() {
-	return ' is okay';
+  return ' is okay';
 }
 
-export function taskNext()
-{
-	let combination = 'Bet
-sometimes let';
-	combination += getLast();
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
 
-	return combination;
+  return combination;
 }
--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,5 +1,3 @@
-export default function
-returnHowManyArguments(...t
-heArgs) {
-	return theArgs.length;
+export default function returnHowManyArguments(...args) {
+  return args.length;
 }
